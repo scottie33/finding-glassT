bf3ce6608e162bdfb5ac18d09b05c1621f09dfc1
scottie33 <scottie.odom@gmail.com>
2013-04-25 16:18:11 +0800

    newer runlist

--- runlist.bash
+++ runlist.bash
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+
+echo " this is a Protocol for Glass Transition Temperature "
 PNUM=8
 EXEC=lmp_g++
 Thigh=600
 Tlow=550
 Tinterval=50
+echo " now you are running ${EXEC}@${PNUM}-cpus [${Thigh}:${Tlow}] cooling by $Tinterval each step."
 
 for i in `echo minimization nvthightemp deformation annealing`; do 
 	#echo "$i.in"
@@ -20,45 +23,68 @@ touch volume-temp.dat
 echo " [ volume-temp.dat ] created for recording volume temperature"
 
 for ((i=${Thigh};i>=${Tlow};i=$i-${Tinterval})); do 
-	echo " running NPT:T=$i now ..." 
-	let lastT=$i+$Tinterval
-	echo "# <- temporarily created for glass-T routine ->" > npttemperature.in
+	#### nvt @ this temperature ####
+	echo " running NVT:T=$i now ..." 
+	let lastT=$i+${Tinterval}
+	let nextT=$i-${Tinterval}
+	echo " # <- temporarily created for glass-T routine ->" > nvttemperature.in
 	if [ $i -eq ${Thigh} ];then
-		echo " loading .in from annealing ... "
-		echo "variable fname index annealing" >> npttemperature.in
+		echo " loading .in from restart.annealing.dat ... "
+		echo " variable fname index annealing" >> nvttemperature.in
 	else
-		echo " loading .in from nvtlowtemp.$lastT ... ".
-		echo "variable fname index nvtlowtemp.$lastT" >> npttemperature.in
-	fi
-	echo "variable simname index npt2zero.$i" >> npttemperature.in
-	echo "variable temperature index $i" >> npttemperature.in
+		echo " loading .in from restart.nvtlowtemp.${lastT}.log ... "
+		echo " variable fname index nvtlowtemp.$lastT" >> nvttemperature.in
+	fi	
+	echo " variable simname index nvttemp.$i" >> nvttemperature.in
+	echo " variable Htemperature index $i" >> nvttemperature.in
+	echo " variable Ltemperature index $i" >> nvttemperature.in
+	cp nvtlowtemp.in nvttemp.$i.in
+	echo " nvttemp.$i.in created:"
+	echo " ----------------------------------"
+	#cat nvttemperature.in
+	echo " ----------------------------------"
+	#mpirun -np ${PNUM} ${EXEC} < nvttemp.$i.in 
+	echo " NVT:T=$i ended successfully."
+	echo " "
+	#### npt @ this temperature ####
+	echo " running NPT:T=$i now ..." 
+	echo " # <- temporarily created for glass-T routine ->" > npttemperature.in
+	echo " loading .in from restart.nvttemp.$i.dat ... "
+	echo " variable fname index nvttemp.$i" >> npttemperature.in
+	echo " variable simname index npt2zero.$i" >> npttemperature.in
+	echo " variable temperature index $i" >> npttemperature.in
 	cp npt2zero.in npt2zero.$i.in
 	echo " npt2zero.$i.in created:"
 	echo " ----------------------------------"
-	cat npttemperature.in
+	#cat npttemperature.in
 	echo " ----------------------------------"
 	#mpirun -np ${PNUM} ${EXEC} < npt2zero.$i.in  
 	echo " ouput volume - temperature ... "
 	cp npt2zero.$i.vol.dat npt2zero.vol.dat
-	#python < avevol.py
+	python < avevol.py
 	cp volume.dat volume.$i.dat
 	cat volume.$i.dat >> volume-temp.dat
-	echo " "
 	echo " NPT:T=$i ended successfully."
-	echo " running NVT:T=$i now ..."  
-	echo "# <- temporarily created for glass-T routine ->" > nvttemperature.in
-	echo " loading .in from npt2zero.$i ... ".
-	echo "variable fname index npt2zero.$i" >> nvttemperature.in
-	echo "variable simname index nvtlowtemp.$i" >> nvttemperature.in
-	echo "variable temperature index $i" >> nvttemperature.in
-	cp nvtlowtemp.in nvtlowtemp.$i.in
-	echo " nvtlowtemp.$i.in created:"
-	echo " ----------------------------------"
-	cat nvttemperature.in
-	echo " ----------------------------------"
-	#mpirun -np ${PNUM} ${EXEC} < nvtlowtemp.$i.in 
-	echo " NVT:T=$i ended successfully."
-	echo ""
+	echo " "
+	#### lower the temperature ####
+	if [ $i -eq ${Tlow} ]; then
+		echo " complete."
+	else
+		echo " # <- temporarily created for glass-T routine ->" > nvttemperature.in
+		echo " loading .in from restart.npt2zero.$i.dat ... "
+		echo " variable fname index npt2zero.$i" >> nvttemperature.in
+		echo " variable simname index nvtlowtemp.$i" >> nvttemperature.in
+		echo " variable Htemperature index $i" >> nvttemperature.in
+		echo " variable Ltemperature index $nextT" >> nvttemperature.in
+		cp nvtlowtemp.in nvtlowtemp.$i.in
+		echo " nvtlowtemp.$i.in created:"
+		echo " ----------------------------------"
+		#cat nvttemperature.in
+		echo " ----------------------------------"
+		#mpirun -np ${PNUM} ${EXEC} < nvtlowtemp.$i.in 
+		echo " NVT:[T=${i}->T=${nextT}] ended successfully."
+		echo " "
+	fi
 done
 
 echo "inp='volume-temp.dat'" > tempdata.gpl
